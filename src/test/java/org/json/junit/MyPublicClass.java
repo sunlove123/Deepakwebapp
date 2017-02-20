a7c4a824d43429715cbd394308f37cc401b87ada
B <425721@CTS.COM>
2017-02-20 12:39:09 +0530

    cleaning up blah blah

--- src/test/java/org/json/junit/MyPublicClass.java
+++
@@ -1,9 +0,0 @@
-package org.json.junit;
-
-/**
- * Need a class with some public data members for testing
- */
-public class MyPublicClass {
-    public Integer publicInt = 42;
-    public String publicString = "abc";
-}
