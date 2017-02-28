d5d58fc4e99ac37ef8562b56082ed6ab15da93f6
sunlove123 <deenadhayalan.b.cts@gmail.com>
2017-02-28 10:53:32 +0530

    commit

---
+++ src/test/java/Test/BasicTestIT.java
@@ -0,0 +1,23 @@
+package Test;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.Test;
+//import org.openqa.selenium.*;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.*; 
+
+public class BasicTestIT {
+  @Test
+  public void f() {
+	 // System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
+	 // WebDriver	driver = new ChromeDriver();
+	 //WebDriver	driver = new FirefoxDriver();
+	 WebDriver	driver = new HtmlUnitDriver();
+	  driver.get("http://localhost:8092/retailone/");
+	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
+	  driver.close();
+  }
+}
---
+++ src/test/java/Test/RegressionTestIT.java
@@ -0,0 +1,33 @@
+package Test;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.*;
+import org.testng.annotations.Test;
+import org.openqa.selenium.htmlunit.*; 
+
+import Page.homePage;
+import Page.loginPage;
+import Page.orderReportPage;
+
+public class RegressionTestIT {
+  @Test
+  public void searchTest() {
+	  
+	  //System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
+	  //WebDriver	driver = new ChromeDriver();
+	 // WebDriver	driver = new FirefoxDriver();
+	 WebDriver	driver = new HtmlUnitDriver();
+	  driver.get("http://localhost:8092/retailone/");
+	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
+	  
+	  loginPage loginpage = new loginPage(driver);
+	  loginpage.validLogin().validSearch().validCheckout().proceedToCheckout().fillAddress().confirmOrder().makePayments();
+	  orderReportPage reportPage = new orderReportPage(driver);
+	  reportPage.orderConfirmation();
+	  loginpage =  reportPage.logout();
+	  driver.close();
+  }
+}
